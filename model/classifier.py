4880223cfb914ac1b51045c0a0abb938223fb180
Tariq Berrada <tariq.berrada@student-cs.fr>
2022-01-20 21:05:06 +0100

    remoce softmax

--- model/classifier.py
+++ model/classifier.py
@@ -7,9 +7,9 @@ class Classifier(torch.nn.Module):
         self.fc1 = torch.nn.Linear(input_dim, hidden_dim)
         self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
         self.out = torch.nn.Linear(hidden_dim, output_dim)
-        self.softmax = torch.nn.Softmax(dim=1)
+        # self.softmax = torch.nn.Softmax(dim=1)
 
     def forward(self, x):
         x = F.leaky_relu(self.fc1(x))
         x = F.leaky_relu(self.fc2(x))
-        return self.softmax(self.out(x))
+        return self.out(x)
